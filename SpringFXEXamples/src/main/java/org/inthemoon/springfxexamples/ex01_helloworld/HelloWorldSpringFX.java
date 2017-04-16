cf4d4010429405b85b19e28f2d170bd4a6359120
Dmitry S Kravchenko <dims12@mail.ru>
2017-04-17 00:04:17 +0300

    comments for HelloWorld example

--- SpringFXEXamples/src/main/java/org/inthemoon/springfxexamples/ex01_helloworld/HelloWorldSpringFX.java
+++ SpringFXEXamples/src/main/java/org/inthemoon/springfxexamples/ex01_helloworld/HelloWorldSpringFX.java
@@ -12,12 +12,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**
+ * Entire application is inside single Spring configuration. This configuration is
+ * used to create context in {@link #main(String[])} method, then application is ran
+ * by {@link SpringFX#launch(String[])} method.</p>
+ *
+ * All components are in the form of Spring beans.
+ *
  * Created by Dims on 25.03.2017.
  */
 @Configuration
 @PropertySource("classpath:helloworld/application.properties")
 public class HelloWorldSpringFX {
 
+   /**
+    * Alert window
+    * @return
+    */
    @Bean
    Alert helloWorldPressedAlert() {
       Alert ans = new Alert(Alert.AlertType.INFORMATION, "Hello world pressed");
@@ -26,6 +36,10 @@ public class HelloWorldSpringFX {
       return ans;
    }
 
+   /**
+    * A button to invoke alert window
+    * @return
+    */
    @Bean
    Button helloWorldButton() {
       Button ans = new Button();
@@ -40,6 +54,10 @@ public class HelloWorldSpringFX {
       return ans;
    }
 
+   /**
+    * Root pane to hold button
+    * @return
+    */
    @Bean
    StackPane root() {
       StackPane ans = new StackPane();
@@ -47,14 +65,27 @@ public class HelloWorldSpringFX {
       return ans;
    }
 
+   /**
+    * Scene
+    * @return
+    */
    @Bean
    Scene scene() {
       return new Scene(root(), 800, 600);
    }
 
+   /**
+    * Starter code
+    * @param args
+    */
    public static void main(String[] args) {
 
+      // get SpringFX class
+      // this class behaves like Spring context factory and receives annotated classes as argument
+      // besides beans from this configuration, it adds another service beans, which rund Java FX
       SpringFX springFX = new SpringFX(HelloWorldSpringFX.class);
+
+      // launching
       springFX.launch(args);
 
    }
